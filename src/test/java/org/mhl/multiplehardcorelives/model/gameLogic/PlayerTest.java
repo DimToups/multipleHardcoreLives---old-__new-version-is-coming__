55e2634aa390fb822b78ddb2612de2a92b564aca
DimToups <dim.saesso@gmail.com>
2025-02-20 17:52:17 +0100

    refactor: Changed the repertories for tests classes

--- src/test/java/org/mhl/multiplehardcorelives/model/gameLogic/PlayerTest.java
+++ src/test/java/org/mhl/multiplehardcorelives/model/gameLogic/PlayerTest.java
@@ -1,4 +1,4 @@
-package org.mhl.multiplehardcorelives.doubles;
+package org.mhl.multiplehardcorelives.model.gameLogic;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
